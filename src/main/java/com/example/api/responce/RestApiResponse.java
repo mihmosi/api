ce58f930b501d5352088b1bce7767b95773d50ab
mihmosi <mihmosi@gmail.com>
2023-12-10 22:01:18 +0300

    make exception handler more informative

---
+++ src/main/java/com/example/api/responce/RestApiResponse.java
@@ -0,0 +1,30 @@
+package com.example.api.responce;
+
+public class RestApiResponse {
+    private int status;
+    private String message;
+    private Object data;
+
+    public RestApiResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public RestApiResponse(int status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
